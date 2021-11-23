8653fd85b7ab031d4f9c3ed1f9d1528c061122fb
anselme <andesanselme@gmail.com>
2021-11-23 09:17:38 +0200

    make the createion of the contacts always generate id

--- src/main/java/com/example/classajunit/model/Contact.java
+++ src/main/java/com/example/classajunit/model/Contact.java
@@ -1,15 +1,13 @@
 package com.example.classajunit.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "contacts")
 public class Contact {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
