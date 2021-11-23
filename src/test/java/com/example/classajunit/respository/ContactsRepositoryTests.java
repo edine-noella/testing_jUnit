7c4f859fc667bd1d8056b83693274df9b98cddcc
anselme <andesanselme@gmail.com>
2021-11-23 09:47:45 +0200

    test the repository

--- src/test/java/com/example/classajunit/respository/ContactsRepositoryTests.java
+++ src/test/java/com/example/classajunit/respository/ContactsRepositoryTests.java
@@ -1,5 +1,64 @@
 package com.example.classajunit.respository;
 
+import com.example.classajunit.model.Contact;
+import com.example.classajunit.repository.IContactRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
 public class ContactsRepositoryTests {
-    // TODO: Implement me
+
+    @Autowired
+    private IContactRepository contactRepository;
+
+    @Test
+    public void findAll_test() {
+        List<Contact> contacts = contactRepository.findAll();
+
+        assertEquals(contacts.size(), 14);
+    }
+
+    @Test
+    public void findById_test() {
+        Optional<Contact> contact = contactRepository.findById(109L);
+
+        if (!contact.isPresent())
+            fail("User with this id is not found");
+
+        assertEquals(contact.get().getMobilePhone(), "0718888881");
+    }
+
+    @Test
+    public void findByMobilePhone() {
+        Optional<Contact> contact = contactRepository.findByMobilePhone("0718888881");
+
+        if (!contact.isPresent())
+            fail("User with this mobile phone number is not found");
+
+        assertEquals(contact.get().getId(), Long.valueOf(109));
+    }
+
+    @Test
+    public void findByWorkPhone() {
+        List<Contact> contacts = contactRepository.findByWorkPhone("0783384212");
+
+        assertEquals(contacts.size(), 3);
+    }
+
+    @Test
+    public void findByHomePhone() {
+        List<Contact> contacts = contactRepository.findByHomePhone(null);
+
+        assertEquals(contacts.size(), 14);
+    }
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,23 @@
-insert into item(id, name, price, quantity) values(101,'Item1',10,100);
-insert into item(id, name, price, quantity) values(102,'Item2',20,200);
-insert into item(id, name, price, quantity) values(103,'Item3',30,300);
-insert into item(id, name, price, quantity) values(104,'Item4',40,400);
+insert into items(id, name, price, quantity)
+values (101, 'Item1', 10, 100);
+insert into items(id, name, price, quantity)
+values (102, 'Item2', 20, 200);
+insert into items(id, name, price, quantity)
+values (103, 'Item3', 30, 300);
+insert into items(id, name, price, quantity)
+values (104, 'Item4', 40, 400);
+
+insert into contacts values (100, null, 'user1@test.test', null, 'Kalisa', null, 'Augustin', null, '0788888881', null, '0783384212');
+insert into contacts values (101, null, 'user2@test.test', null, 'Mugabo', null, 'Dider', null, '0788888882', null, null);
+insert into contacts values (102, null, 'user3@test.test', null, 'Kalisa', null, 'Ellisa', null, '0788888831', null, null);
+insert into contacts values (103, null, 'user4@test.test', null, 'Ismael', null, 'Augustin', null, '0788884881', null, null);
+insert into contacts values (104, null, 'user5@test.test', null, 'Kalisa', null, 'Jacques', null, '0788888581', null, null);
+insert into contacts values (105, null, 'user6@test.test', null, 'Emanuel', null, 'Augustin', null, '0788868881', null, null);
+insert into contacts values (106, null, 'user7@test.test', null, 'Kalisa', null, 'Blassing', null, '0788887881', null, null);
+insert into contacts values (107, null, 'user8@test.test', null, 'Mwizerwa', null, 'Fiston', null, '078838881', null, '0783384212');
+insert into contacts values (108, null, 'user9@test.test', null, 'Kalisa', null, 'Mugabo', null, '0788888891', null, null);
+insert into contacts values (109, null, 'user10@test.test', null, 'Gervais', null, 'Faustin', null, '0718888881', null, null);
+insert into contacts values (110, null, 'user11@test.test', null, 'Manzi', null, 'Kwizera', null, '0782888881', null, null);
+insert into contacts values (111, null, 'user12@test.test', null, 'Kalisa', null, 'Hirwa', null, '0788388881', null, '0783384212');
+insert into contacts values (112, null, 'user13@test.test', null, 'Jackson', null, 'Irakoze', null, '0748888881', null, null);
+insert into contacts values (113, null, 'user14@test.test', null, 'Kalisa', null, 'Egide', null, '0788858881', null, null);
